47b64e66a6e36ce97556580dfc54c471329f3abb
Widi Priyasto <widigie@gmail.com>
2024-02-17 15:36:27 +0700

    push update and error execption on book controller book api

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Books;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use \App\Exceptions\QueryException;
 
 
 class BookController extends Controller
@@ -17,30 +18,75 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
-        $books = New Books;
-        $books->title = $request->title;
-        $books->category = $request->category;
-        $books->author = $request->author;
-        $books->publish_date = $request->publish_date;
-        $books->save();
+        try {
 
-        return response()->json([
-            "message" => "Book Added."
-        ], 201);
+            $books = New Books;
+            $books->title = $request->title;
+            $books->category = $request->category;
+            $books->author = $request->author;
+            $books->publish_date = $request->publish_date;
+            $books->save();
+
+            return response()->json([
+                "message" => "Book Added.",
+                "status" => 200
+            ], 200);
+
+        } catch (\Exception $e) {
+            throw new QueryException($e->getMessage());
+        }
     }
 
     public function show($id)
     {
-        $book = Books::find($id);
-        if(!empty($book))
-        {
-            return response()->json($book);
+        try {
+
+            $book = Books::find($id);
+            if (!empty($book))
+            {
+                return response()->json($book);
+            }
+            else
+            {
+                return response()->json([
+                    "message" => "Book not found",
+                    "status" => 404
+                ], 404);
+            }
+
+        } catch (\Exception $e) {
+            throw new QueryException($e->getMessage());
         }
-        else
-        {
-            return response()->json([
-                "message" => "Book not found"
-            ], 404);
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+
+            if (Books::where('id', $id)->exists())
+            {
+                $book = Books::find($id);
+                $book->title = is_null($request->title) ? $book->title : $request->title;
+                $book->category = is_null($request->category) ? $book->category : $request->category;
+                $book->author = is_null($request->author) ? $book->author : $request->author;
+                $book->publish_date = is_null($request->publish_date) ? $book->publish_date : $request->publish_date;
+                $book->update();
+
+                return response()->json([
+                    "message" => "Book Updated.",
+                    'status' => 200
+                ], 200);
+            }
+            else
+            {
+                return response()->json([
+                    "message" => "Book not found.",
+                    'status' => 404
+                ], 404);
+            }
+
+        } catch (\Exception $e) {
+            throw new QueryException($e->getMessage());
         }
     }
 }
