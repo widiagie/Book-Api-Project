fc2c27b6f609e56f00137eda755b840d426c9b17
Widi Priyasto <widigie@gmail.com>
2024-02-17 10:52:00 +0700

    push register controller book api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
  * route "/register"
  * @method "POST"
  */
-// Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
 /**
  * route "/login"
@@ -30,10 +30,10 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
  * route "/user"
  * @method "GET"
  */
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
